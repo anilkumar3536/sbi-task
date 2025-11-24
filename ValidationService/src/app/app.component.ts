298b5ca50c3207987c91f45333110f19a7b4aac7
anilkumar3536 <anilkumarturla9@gmail.com>
2025-11-24 16:44:09 +0530

    Component developed

--- ValidationService/src/app/app.component.ts
+++ ValidationService/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidationService } from './validation.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ValidationService';
+
+  carpetForm! : FormGroup
+  isfetchdata : any;
+  carpetAreaUnitMap : any;
+
+  areaUnitList = [
+    { carpetAreaUnitId: 1, carpetAreaUnitDesc: "Square metre" },
+    { carpetAreaUnitId: 2, carpetAreaUnitDesc: "Square feet" },
+    { carpetAreaUnitId: 3, carpetAreaUnitDesc: "Acre" },
+    { carpetAreaUnitId: 4, carpetAreaUnitDesc: "Hectare" },
+    { carpetAreaUnitId: 5, carpetAreaUnitDesc: "Gunta" }
+  ];
+
+  fetchdata = [
+    {
+      carpetArea: "362",
+      carpetAreaUnit: 1,
+      carpetAreaUnitMap: [
+        { carpetAreaUnitId: 1, minCarpetAreaAsPerUnit: 60.001, maxCarpetAreaAsPerUnit: 120 },
+        { carpetAreaUnitId: 2, minCarpetAreaAsPerUnit: 645.835, maxCarpetAreaAsPerUnit: 1291.67 },
+        { carpetAreaUnitId: 3, minCarpetAreaAsPerUnit: 0.016, maxCarpetAreaAsPerUnit: 0.03 },
+        { carpetAreaUnitId: 4, minCarpetAreaAsPerUnit: 0.007, maxCarpetAreaAsPerUnit: 0.012 },
+        { carpetAreaUnitId: 5, minCarpetAreaAsPerUnit: 0.594, maxCarpetAreaAsPerUnit: 1.186 }
+      ]
+    }
+  ];
+
+  constructor(){}
+
+  ngOnInit(){
+    this.carpetForm = new FormGroup({
+      carpetarea:new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*\.?[0-9]+$/)]),
+      carpetareaunit : new FormControl()
+    })
+    this.loadData()
+
+  }
+
+  loadData(){
+    this.isfetchdata = this.fetchdata;
+    console.log(this.isfetchdata)
+    this.carpetForm.patchValue({
+      carpetarea : this.isfetchdata[0].carpetArea,
+      carpetareaunit : this.isfetchdata[0].carpetAreaUnit
+    })
+  }
+
 }
--- ValidationService/src/app/validation.service.ts
+++ ValidationService/src/app/validation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,21 @@ import { Injectable } from '@angular/core';
 export class ValidationService {
 
   constructor() { }
+
+  static carpetAreaValidation(min:any, max:any) : ValidatorFn{
+    return (control:AbstractControl) => {
+      let carpetArea = Number(control.value)
+
+      let minValue=min;
+      let maxValue=max;
+      console.log(carpetArea,minValue,maxValue)
+      
+      if(carpetArea<minValue){
+        return {carpetAreaValidation: "Value is less than Minimum value"};
+      }else if(carpetArea>maxValue){
+        return {carpetAreaValidation: "Value is greater than Maximaum value"};
+      }
+      return null;
+    }
+  }
 }
